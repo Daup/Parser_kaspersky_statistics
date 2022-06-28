03ccf83be5724ffa0cb888746a5401e39cc12127
Konstantin <93738471+Daup@users.noreply.github.com>
2022-06-28 07:28:00 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,183 +1,179 @@
-import multiprocessing
-import threading
 import lxml
-import pandas as pd
-import numpy as np
+import pika
+import asyncio
+import json
+from elasticconnector import ElasticConnector
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
-
-# URL_list_of_countries = 'https://statistics.securelist.com/countries'
-URL_list_of_countries_ru = 'https://statistics.securelist.com/ru/countries'
-# списки для графика
-value = []
-data = []
-country = []
-# списки для таблицы угроз
-danger = []
-rang = []
-percent = []
+from natasha import (Segmenter,MorphVocab,NewsEmbedding,NewsMorphTagger,NewsSyntaxParser,NewsNERTagger,PER,NamesExtractor,Doc,DatesExtractor)
 
 
-# get_html - функция для получения html
-def get_html(url):
-    r = urlopen(url=url).read().decode('UTF-8')
-    return r
+class IpiadHabr:
 
+    def __init__(self):
+        self.url_head = 'https://habr.com/ru/news/page1/'
+        self.list_author = []
+        self.list_link = []
+        self.list_heading = []
+        self.list_data = []
+        self.list_text = []
+        self.list_page_link = []
+        self.range_page = 0
 
-# get_link_countries - функция для получения ссылок стран
-def get_link_countries(url=URL_list_of_countries_ru):
-    soup = BeautifulSoup(get_html(url), 'lxml')
-    list_table = soup.find_all(name='a', class_='card')
-    list_link_countries = []
-    list_link_countries_month = []
+    def __str__(self):
+        return f'url_head: {self.url_head} \n' \
+               f'list_author: {self.list_author} \n' \
+               f'list_link: {self.list_link} \n' \
+               f'list_heading: {self.list_heading} \n' \
+               f'list_data: {self.list_data} \n' \
+               f'list_text: {self.list_text} \n' \
+               f'list_page_link: {self.list_page_link} \n' \
+               f'range page: {self.range_page} \n'
 
-    for item in list_table:
-        list_link_countries.append(item.get('href'))
+    @staticmethod
+    def get_html(url):
+        r = urlopen(url=url).read().decode('UTF-8')
+        return r
 
-    for item in list_link_countries:
-        i = item[:-3] + 'month'
-        list_link_countries_month.append(i)
+    def get_range_page(self):
+        soup = BeautifulSoup(self.get_html(self.url_head), 'lxml')
+        range_page = soup.find(name='div', class_='tm-pagination__pages')
+        list = []
+        for item in range_page.select('a'):
+            item = int(item.get_text())
+            list.append(item)
+        self.range_page = max(list)
 
-    return list_link_countries_month
+        for item in range(1, self.range_page + 1):
+            link = f'https://habr.com/ru/news/page{item}/'
+            self.list_page_link.append(link)
 
+        return self.range_page
 
-# get_country - функция для получения названия страны
-def get_country(url):
-    countries = str(url)
-    countries = countries.replace('https://statistics.securelist.com/ru/country/', '')
-    countries = countries.replace('/on-access-scan/month', '')
-    countries = countries.replace('%20', '_')
-    countries = countries.replace('/intrusion-detection-scan/month', '')
-    return countries
+    async def get_author(self, link):
+        soup = BeautifulSoup(self.get_html(link), 'lxml')
+        author = soup.find_all(name='a', class_='tm-user-info__username')
+        for item in author:
+            item = item.get_text().strip()
+            self.list_author.append(item)
+        return self.list_author
 
+    async def get_link_and_heading(self, link):
+        # soup = BeautifulSoup(self.get_html(self.url_head), 'lxml')
+        soup = BeautifulSoup(self.get_html(link), 'lxml')
+        link = soup.find_all(name='a', class_='tm-article-snippet__title-link')
+        for item in link:
+            item = f"{item['href']}"
+            item = f'https://habr.com{item}'
+            self.list_link.append(item)
 
-# get_value_table - функция для получения значения таблицы (значения будут получены, как один единый список)
-def get_value_table(url):
-    soup = BeautifulSoup(get_html(url), 'lxml')
-    table = soup.find(name='div', class_='container', id='chart-wrapper')
-    meaning = table.get('data-chart')
-    meaning = meaning.replace('[', '')
-    meaning = meaning.replace(']', '')
-    meaning = meaning.replace('{', '')
-    meaning = meaning.replace('}', '')
-    meaning = meaning.replace('"', '')
-    meaning = meaning.replace('value:', '')
-    meaning = meaning.replace('date:', '')
-    meaning = meaning.split(',')
-    return meaning
+        for item in link:
+            self.list_heading.append(item.get_text())
 
+    async def get_data(self, link):
+        # soup = BeautifulSoup(self.get_html(self.url_head), 'lxml')
+        soup = BeautifulSoup(self.get_html(link), 'lxml')
+        data = soup.find_all(name='time')
+        for item in data:
+            item = item.get_text()
+            self.list_data.append(item)
 
-# get_danger_table - функция для получения значений (топ-10 обнаруженных угроз)
-def get_danger_table(url):
-    soup = BeautifulSoup(get_html(url), 'lxml')
-    table = soup.find_all(name='div', class_='p-2 d-flex align-items-center')
-    danger = []
-    rang = []
-    percent = []
-    for item in table:
-        danger += item.find(name='span')
-        percent += item.find(name='div', class_='ml-auto list-value')
-    count = len(danger)
-    country = [get_country(url)]
-    if count != 0:
-        for i in range(1, count + 1):
-            rang.append(i)
-    else:
-        danger = ['nope']
-        rang = ['0']
-        percent = ['0']
+    async def get_text(self, link):
+        soup = BeautifulSoup(self.get_html(link), 'lxml')
+        text = soup.find_all(name='p')
+        for item in text:
+            item = item.get_text()
+            self.list_text.append(item)
 
-    return rang, danger, percent, country
+    async def func_main(self, list_page_link):
+        chunk = 5
+        tasks = []
+        for item in list_page_link:
+            # await self.get_author(item)
+            # await self.get_link_and_heading(item)
+            # await self.get_data(item)
+            tasks.append(asyncio.create_task(self.get_author(item)))
+            tasks.append(asyncio.create_task(self.get_link_and_heading(item)))
+            tasks.append(asyncio.create_task(self.get_data(item)))
+            if len(tasks) == chunk:
+                await asyncio.gather(*tasks)
+                tasks = []
+            # await asyncio.gather(task_author,task_link,task_data)
+            # self.list_page_link.remove(item)
 
+    async def func_main_text(self, list_link):
+        chunk = 5
+        tasks = []
+        for item in list_link:
+            tasks.append(asyncio.create_task(self.get_text(item)))
+            if len(tasks) == chunk:
+                await asyncio.gather(*tasks)
+                tasks = []
 
-# len_data - функция счета размеров таблицы для каждой страны
-def len_data():
-    number = 0
-    list_data = get_link_countries()
-    for item in list_data:
-        print('index: {} len {}'.format(number, len(get_value_table(item))))
-        number += 1
+    def identification(self, text):
+        segmenter = Segmenter()
+        emb = NewsEmbedding()
+        morph_tagger = NewsMorphTagger(emb)
+        syntax_parser = NewsSyntaxParser(emb)
+        # ner_tagger = NewsNERTagger(emb)
 
+        doc = Doc(text)
+        doc.segment(segmenter)
+        doc.tag_morph(morph_tagger)
+        doc.parse_syntax(syntax_parser)
+        # doc.tag_ner(ner_tagger)
+        return doc.tokens[:3]
+        # return doc.sents[:5]
 
-# work_with_xlsx - функция для получения value, data, country
-# item - список полученный в функции  get_value_table
-def work_with_xlsx(item, url):
-    len_item = len(item)
-    country = [get_country(url)]
-    if len_item == 1:
-        value = ['no data']
-        data = ['2000-01-01 00:00']
-    else:
-        value = item[::2]
-        data = item[1::2]
-
-    return value, data, country
-
-
-def main_def(urls):
-    item = get_value_table(urls)
-    value_i, data_i, country_i = work_with_xlsx(item, urls)
-    return value_i, data_i, country_i
-
-
-# end_def - функция записи значений в список value, data, country
-def end_def(response):
-    global value, data, country
-    count = []
-    for i in response[0]:
-        value.append(i)
-        count.append(i)
-    for i in response[1]:
-        data.append(i)
-    country += response[2] * len(count)
-
-
-def end_danger_def(response):
-    global rang, danger, percent, country
-    count = []
-    print(response)
-    for i in response[0]:
-        danger.append(i)
-        count.append(i)
-    for i in range(1, len(count) + 1):
-        rang.append(i)
-    for i in response[1]:
-        percent.append(i)
-    country += response[2] * len(count)
 
 if __name__ == '__main__':
-    work = 0
-    if work == 1:
-        list = get_link_countries()
-        df = pd.read_excel('D:\Project\parserForNirs\dataframe.xlsx')
+    news = IpiadHabr()
+    news.get_range_page()
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(news.func_main(list_page_link=news.list_page_link))
+    loop.run_until_complete(news.func_main_text(list_link=news.list_link))
 
-        with multiprocessing.Pool(multiprocessing.cpu_count() * 3) as p:
-            for i in list:
-                p.apply_async(main_def, args=(i,), callback=end_def)
-            p.close()
-            p.join()
+    list_json = json.dumps([news.list_author, news.list_link, news.list_heading, news.list_data, news.list_text])
+    credentials = pika.PlainCredentials('guest', 'guest')
+    parameters = pika.ConnectionParameters('localhost', 5672, '/', credentials)
+    connection = pika.BlockingConnection(parameters)
+    channel = connection.channel()
+    channel.queue_declare(queue='link')
+    channel.basic_publish(exchange='',
+                          routing_key='link',
+                          body=list_json)
+    
 
-        df = pd.DataFrame({'value': value, 'data': data, 'country': country})
-        df.to_excel('D:\Project\parserForNirs\dataframe.xlsx', index=False)
-    else:
-        list = get_link_countries()
-        print(list)
-        df = pd.read_excel('D:\Project\parserForNirs\dataframe_danger.xlsx')
-        #with multiprocessing.Pool(multiprocessing.cpu_count() * 3) as p:
-        #    for i in list:
-        #        p.apply_async(get_danger_table, args=(i,), callback=end_danger_def)
-        #    p.close()
-        #    p.join()
-        for i in list:
-            rang_i, danger_i, percent_i, country_i = get_danger_table(i)
-            for i in rang_i:
-                rang.append(i)
-            for i in danger_i:
-                danger.append(i)
-            for i in percent_i:
-                percent.append(i)
-            country += country_i * len(danger_i)
+    def RecvMsg():
 
-        df = pd.DataFrame({'rang': rang, 'name_danger': danger, 'percent': percent, 'country': country})
-        df.to_excel('D:\Project\parserForNirs\dataframe_danger.xlsx', index=False)
+        connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost', port=5672,
+                                                                       credentials=pika.PlainCredentials('guest',
+                                                                                                         'guest')))
 
+        def callback(ch, method, properties, body):
+            print("Received[*] ")
+            ch.basic_ack(delivery_tag=method.delivery_tag)
+            jsonobj = json.loads(body.decode("utf-8"))
+            print(jsonobj)
+            array1 = jsonobj[0]
+            array2 = jsonobj[1]
+            array3 = jsonobj[2]
+            array4 = jsonobj[3]
+            array5 = jsonobj[4]
+            es = ElasticConnector()
+
+            for item in range(0, len(jsonobj[0])):
+                json_item = {'author': array1[item],
+                             'link': array2[item],
+                             'heading': array3[item],
+                             'data': array4[item],
+                             'text': array5[item]}
+                es.AppendNew(json_item)
+            return jsonobj
+
+        channel = connection.channel()
+        channel.queue_declare(queue='link')
+        channel.basic_consume(on_message_callback=callback, queue="link")
+        channel.start_consuming()
+
+    RecvMsg()
